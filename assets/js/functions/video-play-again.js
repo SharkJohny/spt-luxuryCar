eec4a1bfbd27ff01e2acd5d28ac7d14e3005eb5d
Jan Kučera <93750764+SharkJohny@users.noreply.github.com>
2025-06-19 00:19:44 +0200

    update

--- assets/js/functions/video-play-again.js
+++ assets/js/functions/video-play-again.js
@@ -3,14 +3,15 @@
  * - Přehrávání/pauzování videa kliknutím
  * - Tlačítko "Přehrát znovu" na konci videa
  * - Zabránění přehrávání více videí současně
+ * - Nově: podpora pro .wrapper > video na homepage
  */
 export function initVideoPlayAgain() {
   // Odstraníme existující handlery
-  jQuery("video.desctop, video.mobile").off("click.videoControl ended.videoControl play.videoControl pause.videoControl");
+  jQuery("video.desctop, video.mobile, .wrapper > video").off("click.videoControl ended.videoControl play.videoControl pause.videoControl");
 
   // Funkce pro zastavení všech ostatních videí
   function pauseOtherVideos(currentVideo) {
-    jQuery("video.desctop, video.mobile").each(function () {
+    jQuery("video.desctop, video.mobile, .wrapper > video").each(function () {
       if (this !== currentVideo && !this.paused) {
         this.pause();
       }
@@ -18,23 +19,28 @@ export function initVideoPlayAgain() {
   }
 
   // Aplikujeme nové handlery na všechna videa
-  jQuery("video.desctop, video.mobile").each(function () {
+  jQuery("video.desctop, video.mobile, .wrapper > video").each(function () {
     const $video = jQuery(this);
     const $container = $video.parent();
     const videoEl = $video[0];
 
-    // Zobrazujeme jen to video, které má být viditelné (podle viewportu)
-    const isMobile = window.innerWidth < 768;
-    if ((isMobile && $video.hasClass("desctop")) || (!isMobile && $video.hasClass("mobile"))) {
-      $video.css("display", "none");
-      return; // Přeskočíme zpracování tohoto videa
+    // Pokud je video ve wrapperu, povol vždy (homepage)
+    if ($container.hasClass("wrapper")) {
+      $video.show();
+    } else {
+      // ...původní podmínka pro desctop/mobile...
+      const isMobile = window.innerWidth < 768;
+      if ((isMobile && $video.hasClass("desctop")) || (!isMobile && $video.hasClass("mobile"))) {
+        $video.css("display", "none");
+        return; // Přeskočíme zpracování tohoto videa
+      }
     }
 
     // Najdeme nebo vytvoříme play/pause tlačítko
     let $playPauseBtn = $container.find(".playpause");
     if ($playPauseBtn.length === 0) {
       $playPauseBtn = jQuery('<div class="playpause"></div>');
-      $container.after($playPauseBtn);
+      $container.append($playPauseBtn);
     }
 
     // Funkce pro aktualizaci stavu tlačítka podle stavu videa
@@ -53,7 +59,6 @@ export function initVideoPlayAgain() {
 
     // Kliknutí na video - přehrát/zastavit
     $video.on("click.videoControl", function (e) {
-      console.log("Video clicked");
       e.stopPropagation();
       if (videoEl.paused) {
         pauseOtherVideos(videoEl);
@@ -67,18 +72,14 @@ export function initVideoPlayAgain() {
     $playPauseBtn.on("click.videoControl", function (e) {
       e.stopPropagation();
       e.preventDefault();
-
       if (videoEl.ended) {
-        // Pokud video skončilo, přehraj znovu
         pauseOtherVideos(videoEl);
         videoEl.currentTime = 0;
         videoEl.play();
       } else if (videoEl.paused) {
-        // Pokud je zastavené, přehraj ho
         pauseOtherVideos(videoEl);
         videoEl.play();
       } else {
-        // Pokud běží, zastav ho
         videoEl.pause();
       }
       return false;
@@ -89,9 +90,7 @@ export function initVideoPlayAgain() {
       pauseOtherVideos(videoEl);
       updateButtonState();
     });
-
     $video.on("pause.videoControl ended.videoControl", updateButtonState);
-
     // Nastavení počátečního stavu
     updateButtonState();
   });
