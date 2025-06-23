4d094f3cf28811783652907b10b4a89ca2e77880
Jan Kučera <93750764+SharkJohny@users.noreply.github.com>
2025-06-23 23:37:43 +0200

    aa

--- assets/js/components/index.js
+++ assets/js/components/index.js
@@ -152,6 +152,50 @@ export function intIndex() {
     ],
   });
 
+  setTimeout(function () {
+    $(".customers-video").slick({
+      dots: true,
+      centerMode: false,
+      infinite: true,
+      slidesToShow: 3,
+      slidesToScroll: 1,
+      autoplay: false,
+      autoplaySpeed: 4000,
+      arrows: true,
+      responsive: [
+        {
+          breakpoint: 1480,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            arrows: true,
+          },
+        },
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 3,
+          },
+        },
+        {
+          breakpoint: 770,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+        {
+          breakpoint: 350,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1,
+          },
+        },
+      ],
+    });
+  }, 1000);
+
   $("button.text-with-icon.group").on("click", function () {
     $("section#comparison").toggleClass("show-more");
     if (!$("button.text-with-icon.group.less")[0]) {
