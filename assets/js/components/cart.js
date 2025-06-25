4921848ec5684dc8de424e2232d7143faaa6dcae
Jan Kučera <93750764+SharkJohny@users.noreply.github.com>
2025-06-25 15:22:16 +0200

    update

--- assets/js/components/cart.js
+++ assets/js/components/cart.js
@@ -5,6 +5,10 @@ export function initCart(setupData) {
     $(".cart-content.summary-wrapper").appendTo("div#cart-wrapper .col-md-8");
     $(".p-label:contains(Cena za m. j.)").text("Cena za set");
     changeDescription();
+    chechCupon();
+    document.addEventListener("ShoptetDOMContentLoaded", function () {
+      chechCupon();
+    });
   }
 }
 
@@ -21,4 +25,30 @@ function changeDescription() {
     console.log(text);
     $(this).html(newText);
   });
+}
+
+function chechCupon() {
+  console.log("Checking coupon code in cart -----------------------");
+  const getCode = shoptetData.cartInfo.discountCoupon.code;
+  let chechCupon = false;
+  if (getCode == "LUX10") {
+    console.log("Checking coupon code:", getCode);
+    $(".main-link-surcharges").each(function () {
+      const $this = $(this);
+      if (
+        $this.text().includes("Farba boxov ") ||
+        $this.text().includes("autokoberce do kufru - Jednoduché") ||
+        $this.text().includes("Kompletní ochrana")
+      ) {
+        console.log("Coupon found in surcharge:", $this.text());
+        chechCupon = true;
+      }
+    });
+    // $(".applied-coupon input.btn.btn-sm.btn-primary").click();
+  }
+
+  if (!chechCupon) {
+    console.log("Coupon code is not valid, applying changes");
+    $(".applied-coupon input.btn.btn-sm.btn-primary").click();
+  }
 }
